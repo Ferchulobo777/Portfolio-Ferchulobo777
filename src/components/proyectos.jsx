bbca62ceda3b193a32207ee3c55cf6851529bdc1
Fernando Rodriguez <ferchulobo2015@gmail.com>
2023-04-04 21:42:32 -0300

    update

--- src/components/proyectos.jsx
+++ src/components/proyectos.jsx
@@ -3,28 +3,28 @@ const proyectos = [
     title: "Weather App",
     description:
       "Weather App es una aplicación web que te permite conocer tu clima y el de cualquier ciudad del mundo.",
-    image: "./images/weather.webp",
+    image: "/images/weather.png",
     link: "https://wheatherapp-a.netlify.app/",
   },
   {
     title: "Rick and Morty",
     description:
       "esta aplicación te permite conocer los personajes de la serie Rick and Morty, puedes buscarlos por el nombre de su galaxia.",
-    image: "./images/rick.webp",
+    image: "/images/rick.png",
     link: "https://rick-and-morty-app-ferchulobo777.netlify.app/",
   },
   {
     title: "Users CRUD",
     description:
       "simula un CRUD de usuarios, puedes crear, editar y eliminar usuarios.",
-    image: "./images/users.webp",
+    image: "/images/users.png",
     link: "https://crud-ferchulobo777.netlify.app/",
   },
   {
     title: "Poke App",
     description:
       "¿Tienes alguna duda sobre algún pokemon?, esta aplicación te permite conocer todos los pokemones, sus tipos, sus habilidades y mucho más.",
-    image: "./images/poke.webp",
+    image: "/images/poke.png",
     link: "https://pokedex-ferchulobo777.netlify.app/",
   },
   {
